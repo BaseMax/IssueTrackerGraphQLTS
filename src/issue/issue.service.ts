fb88a02eb052388f30615410d49fc7f3188215f5
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-07-31 21:32:18 +0330

    creat findOne service

--- src/issue/issue.service.ts
+++ src/issue/issue.service.ts
@@ -1,26 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { StatusResult } from 'src/common/entities/status-result';
+import { FindOptionsWhere, Repository } from 'typeorm';
 import { CreateIssueInput } from './dto/create-issue.input';
 import { UpdateIssueInput } from './dto/update-issue.input';
+import { Issue } from './entities/issue.entity';
+
+type Where = FindOptionsWhere<Issue>
 
 @Injectable()
 export class IssueService {
-  create(createIssueInput: CreateIssueInput) {
-    return 'This action adds a new issue';
+
+  constructor(
+    @InjectRepository(Issue)
+    private readonly issueRepo:Repository<Issue>
+  ){}
+  async create(createIssueInput:CreateIssueInput):Promise<StatusResult>{
+    let {  } = createIssueInput
   }
 
   findAll() {
     return `This action returns all issue`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} issue`;
+  async findOne(where:Where):Promise<Issue>{
+    const issue = await this.issueRepo.findOne({where}) ;
+
+    if(!issue){
+      throw new NotFoundException('issue not found')
+    }
+
+    return issue ; 
   }
 
-  update(id: number, updateIssueInput: UpdateIssueInput) {
-    return `This action updates a #${id} issue`;
+  update(id: string, updateIssueInput: UpdateIssueInput) {
+    
   }
 
   remove(id: number) {
     return `This action removes a #${id} issue`;
   }
 }
+
