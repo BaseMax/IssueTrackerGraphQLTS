9ef61e21b2f650ac3fb88750190b9ce90febab4d
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-07-31 21:33:26 +0330

    change fields

--- src/issue/dto/create-issue.input.ts
+++ src/issue/dto/create-issue.input.ts
@@ -1,7 +1,36 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
+import { IsNotEmpty } from 'class-validator';
+import { Priority } from '../enums/priority.enum';
+import { Status } from '../enums/status.enum';
 
 @InputType()
 export class CreateIssueInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(()=>String)
+  @IsNotEmpty()
+  title : string ; 
+
+  @Field(()=>String)
+  @IsNotEmpty()
+  description : string ; 
+
+  @Field(()=>String)
+  @IsNotEmpty()
+  priority : Priority ;
+
+  @Field(()=>String)
+  @IsNotEmpty()
+  status : Status;
+
+  @Field(()=>String)
+  @IsNotEmpty()
+  assignee : string ; 
+
+
+  @Field(()=>[String])
+  @IsNotEmpty()
+  labels : string[] ; 
+
+  @Field(()=>[String])
+  @IsNotEmpty()
+  tags : string[];  
 }
