bbcd4a8193e1fd5b13a43ee5b41c9d943de57a18
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-07-31 21:31:51 +0330

    the add issue entity (columns)

--- src/issue/entities/issue.entity.ts
+++ src/issue/entities/issue.entity.ts
@@ -1,8 +1,47 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int, ID } from '@nestjs/graphql';
+import { Comment } from 'src/comment/entities/comment.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Priority } from '../enums/priority.enum';
+import { Status } from '../enums/status.enum';
 
 @ObjectType()
+@Entity('Issue')
 export class Issue {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(()=>ID)
+  @PrimaryGeneratedColumn('uuid')
+  id : string ; 
+
+  @Field(()=>String)
+  @Column({ type : 'varchar'})
+  title : string ; 
+
+  @Field(()=>String)
+  @Column({ type : 'varchar'})
+  description : string ; 
+
+  @Field(()=>String)
+  @Column({ type : 'enum' , enum : Priority , nullable : false })
+  priority : Priority ;
+
+  @Field(()=>String)
+  @Column({ type : 'enum' , enum : Status , array : true ,nullable : false })
+  status : Status;
+
+  @Field(()=>String)
+  @Column({ type : 'varchar'})
+  assignee : string ; 
+
+
+  @Field(()=>[String])
+  @Column({ type : 'varchar' , array : true })
+  labels : string[] ; 
+
+  @Field(()=>[String])
+  @Column({ type : 'varchar' , array : true})
+  tags : string[];
+
+  @Field(()=>[Comment])
+  @OneToMany(()=>Comment , (commnet)=>commnet.issue)
+  comments : Comment[]
 }
 
