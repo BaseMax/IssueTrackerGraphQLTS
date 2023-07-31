ff96b88b39235f76d951d4debd0274cb750401a2
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-07-31 14:39:16 +0330

    add common

---
+++ src/common/entities/status-result.ts
@@ -0,0 +1,13 @@
+import { Field, ObjectType } from "@nestjs/graphql";
+
+@ObjectType()
+export class StatusResult {
+    @Field(()=>String)
+    message : string ; 
+
+    @Field(()=>Boolean)
+    success : boolean ; 
+
+    @Field(()=>String)
+    id? : string  ; 
+}
