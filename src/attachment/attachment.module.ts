1c96f7fc378e7146fc37de19a2645440e3a09837
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-01 23:54:11 +0330

    add issue module

--- src/attachment/attachment.module.ts
+++ src/attachment/attachment.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AttachmentService } from './attachment.service';
 import { AttachmentResolver } from './attachment.resolver';
+import { IssueModule } from 'src/issue/issue.module';
 
 @Module({
+  imports : [
+    IssueModule , 
+  ] , 
   providers: [AttachmentResolver, AttachmentService]
 })
 export class AttachmentModule {}
