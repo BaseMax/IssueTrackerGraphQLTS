69a38a0a71e62db8ee8e64c3b61f14237e8e3e5d
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-01 21:40:10 +0330

    export issue service

--- src/issue/issue.module.ts
+++ src/issue/issue.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { IssueService } from './issue.service';
 import { IssueResolver } from './issue.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Issue } from './entities/issue.entity';
 
 @Module({
-  providers: [IssueResolver, IssueService]
+  imports : [TypeOrmModule.forFeature([Issue])] ,
+  providers: [IssueResolver, IssueService] , 
+  exports : [IssueService]
 })
 export class IssueModule {}
