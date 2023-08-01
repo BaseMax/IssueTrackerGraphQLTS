788337682381ed217434177ca2738f249d65e53a
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-01 21:38:27 +0330

    clean unknown code

--- src/comment/dto/create-comment.input.ts
+++ src/comment/dto/create-comment.input.ts
@@ -2,8 +2,7 @@ import { InputType, Int, Field } from '@nestjs/graphql';
 import { IsNotEmpty } from 'class-validator';
 
 @InputType()
-export class CreateCommentInput {
-  
+export class CreateCommentInput {  
   @Field(()=>String)
   @IsNotEmpty()
   content : string ; 
