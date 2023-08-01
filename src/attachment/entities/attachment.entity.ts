da8cd58d73ba32c97badd33dc2ee839525c3724b
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-01 22:55:05 +0330

    create relation with issue

--- src/attachment/entities/attachment.entity.ts
+++ src/attachment/entities/attachment.entity.ts
@@ -1,7 +1,23 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int, ID } from '@nestjs/graphql';
+import { Issue } from 'src/issue/entities/issue.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity('Attachment')
 export class Attachment {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(()=>ID)
+  @PrimaryGeneratedColumn('uuid')
+  id : string ; 
+
+  @Field()
+  @Column({type : 'varchar' , nullable : false})
+  fileUrl : string ; 
+
+  @Field()
+  @Column({type : 'varchar'})
+  description : string ; 
+
+  @Field(()=>Issue)
+  @ManyToOne(()=>Issue , (issue)=>issue.attachments)
+  issue : Issue ; 
 }
