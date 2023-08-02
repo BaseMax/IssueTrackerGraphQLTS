7cac7dee0e142119c81c1e0da7b806d1c6763686
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-02 14:13:33 +0330

    add entity to typeorm

--- src/attachment/attachment.module.ts
+++ src/attachment/attachment.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AttachmentService } from './attachment.service';
 import { AttachmentResolver } from './attachment.resolver';
 import { IssueModule } from 'src/issue/issue.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Attachment } from './entities/attachment.entity';
 
 @Module({
   imports : [
+    TypeOrmModule.forFeature([Attachment]) ,
     IssueModule , 
   ] , 
   providers: [AttachmentResolver, AttachmentService]
