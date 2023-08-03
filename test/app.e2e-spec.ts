9b90b4a491eb4fd60325320eee7819b52f5f30d5
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-03 18:46:56 +0330

    add issue tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,87 +2,173 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication  } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-import { Repository } from 'typeorm';
-import { getRepositoryToken } from '@nestjs/typeorm';
-import { Issue } from 'src/issue/entities/issue.entity';
-import { Comment } from 'src/comment/entities/comment.entity';
-import { Attachment } from 'src/attachment/entities/attachment.entity';
 import { Priority } from 'src/issue/enums/priority.enum';
 import { Status } from 'src/issue/enums/status.enum';
+import { Repository } from 'typeorm';
+import { Issue } from 'src/issue/entities/issue.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
-describe('AppController (e2e)', () => {
+describe('app resolvers (e2e)', () => {
   let app: INestApplication;
-  let issueRepo : Repository<Issue> ; 
-  let commentRepo : Repository<Comment> ; 
-  let attachmentRepo : Repository<Attachment>  
-
+  let issueRepo:Repository<Issue>; 
+  let parentIssue:Issue ; 
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        AppModule , 
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
-    issueRepo = app.get<Repository<Issue>>(getRepositoryToken(Issue));
-    commentRepo = app.get<Repository<Comment>>(getRepositoryToken(Comment));
+    issueRepo = moduleFixture.get(getRepositoryToken(Issue));
+
+
+    const issue = await issueRepo.create({
+      title : "issue for test" , 
+      description : "create test for issue module" , 
+      assignee : "test" , 
+      status : Status.CLOSE , 
+      priority : Priority.HIGH , 
+      labels : [] ,
+      tags : []
+    })
+
+    parentIssue = await issueRepo.save(issue) ;
     await app.init();
   });
 
   afterAll(async ()=>{
     await app.close()
+    await issueRepo.clear()
   })
 
-
-  describe("issue", () => {
-    it("must create issue", async () => {
-      const mutation = `mutation createIsssue($createIssueInput: createIssueInput!) {
-        createIssue(createIssueInput: $createIssueInput) {
-          success , 
-          message , 
-          id  
-        }
-      }`;
-      const variables = {
-        createIssueInput : {
-          title : 'test title' , 
-          description : 'test description' , 
-          assignee : 'test' , 
-          labels : ['test lables' , 'labe'] , 
-          tags : ['tags' , 'test'] , 
-          priority : Priority.LOW , 
-          status : Status.CLOSE ,
-        }
-      };
-
-      const response = await request(app.getHttpServer())
-        .post("/graphql")
-        .send({
-          query: mutation,
-          variables,
-        });
-
-      expect(response.body.success).toBe(true);
-      
-    });
-
-    it("must find All", async () => {
-      const query = `query {
-        findAllIssue {
-          id, 
-          title , 
-          description , 
-          tags , 
-          lables , 
-        }
-      }`;
-
-      const response = await request(app.getHttpServer())
-        .post("/graphql")
-        .send({
-          query: query
-        });
-
-      expect(response.status).toBe(200);
+  describe('issue' , ()=>{
+    it('find all issue' , async ()=>{
+      return request(app.getHttpServer())
+      .post('/graphql')
+      .send({
+        query : `
+          {
+            findAllIssue {
+              id , 
+              title , 
+              description , 
+            }
+          }
+        `
+      })
+      .expect(200)
+      .expect(({ body }) => {
+        const issues = body.data.findAllIssue;
+        expect(issues).toBeInstanceOf(Array);
+        expect(issues.length).toBeGreaterThan(0);
       });
-  });
-});
+    })
+
+    it('create issue' , async()=>{
+
+      const mutation = `mutation createIssue ($createIssueInput: CreateIssueInput!) {
+          createIssue (createIssueInput: $createIssueInput) {
+              message
+              success
+              id
+          }
+      }`
+
+      const variables = {
+        createIssueInput: {
+          title: "test",
+          description: "test",
+          priority: Priority.HIGH,
+          status : Status.CLOSE , 
+          assignee: "test",
+          labels: [
+            "test"
+          ],
+          tags: [
+            "test-tag"
+          ]
+        }
+      }
+
+      return request(app.getHttpServer())
+        .post('/graphql')
+        .send({
+          query: mutation , 
+          variables 
+        })
+        .expect(200)
+        .expect(({ body }) => {
+          const comment = body.data.createIssue;
+          expect(comment.success).toBe(true);
+        });
+      });
+
+    })
+
+
+    it('find issue by id' , async ()=>{
+      const query = `
+        query findOneIssue ($id: String!) {
+          findOneIssue (id: $id) {
+              id
+              title
+              description
+              priority
+              status
+              assignee
+              labels
+              tags
+          }
+      }
+      `
+
+      const variables = {
+        id : parentIssue.id ,
+      }
+      return request(app.getHttpServer())
+      .post('/graphql')
+      .send({
+        query : query , 
+        variables , 
+      })
+      .expect(200)
+      .expect(({ body }) => {
+        const issue = body.data.findOneIssue;
+        expect(issue).toBeDefined();
+        expect(issue.title).toBe(parentIssue.title);
+      });
+    })
+
+    it('update issue' , async ()=>{
+      const mutation = `
+        mutation updateIssue ($updateIssueInput: UpdateIssueInput!) {
+          updateIssue (updateIssueInput: $updateIssueInput) {
+              message
+              success
+              id
+          }
+      }
+      `
+
+      const variables = {
+        updateIssueInput: {
+          title: "test update issue",
+          id: parentIssue.id , 
+        }
+      }
+
+      return request(app.getHttpServer())
+      .post('/graphql')
+      .send({
+        query : mutation , 
+        variables , 
+      })
+      .expect(200)
+      .expect(({ body }) => {
+        const result = body.data.updateIssue;
+        expect(result.success).toBe(true);
+      });
+    })
+})
